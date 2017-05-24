8af95b778c9afb79a9d7e208e3aa6962d527c3dd
joshNM <josh@northernmediauk.com>
2017-05-24 14:19:01 +0100

    added main.js file for Google map

--- functions.php
+++ functions.php
@@ -124,7 +124,7 @@ function northern_media_scripts() {
 
 	wp_enqueue_script( 'matchheight', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.0/jquery.matchHeight-min.js', array(), '1', true );
 
-	wp_enqueue_script( 'northern_media-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/main.js', array('jquery'), '1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+
+
+    // MAP JS
+    $('.map-container').click(function(){
+        $(this).find('iframe').addClass('clicked')}).mouseleave(function(){
+        $(this).find('iframe').removeClass('clicked')
+    });
+
+})
