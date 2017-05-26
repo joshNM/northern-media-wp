8735076ce0b136b27d030d7ed8e95f02a836c7ae
joshNM <josh@northernmediauk.com>
2017-05-26 10:11:42 +0100

    style employee news

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     // NEWS MATCH HEIGHT
     $('#latest-news .latest-news-inner').matchHeight();
-    $('#employee-news .employee-article').matchHeight();
+    $('#employee-news .col-md-4').matchHeight();
 
     // MAP JS
     $('.map-container').click(function(){
