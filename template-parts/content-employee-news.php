77a5bd27f718fb1dccb8cdf90c2eb758a76fda8c
joshNM <josh@northernmediauk.com>
2017-05-26 09:37:44 +0100

    add employee news to team page

---
+++ template-parts/content-employee-news.php
@@ -0,0 +1,31 @@
+<section id="employee-news">
+    <div class="container">
+        <div class="center"><h2 class="section-title">Employee News</h2></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi.</p>
+
+        <div class="row">
+
+            <?php 
+                $employeeArgs = [
+                    'post_type' => 'post',
+                    'category__in' => 18
+                ];
+
+                $employeeArticle = new WP_Query($employeeArgs);
+            ?>
+            <?php while($employeeArticle->have_posts()) : ?>
+                <div class="col-md-4">
+                    <div class="employee-article">
+                    <img src="<?php the_post_thumbnail_url(); ?>">
+         
+                    </div>
+                </div>
+            <?php endwhile; ?>
+
+
+        </div>
+
+    </div>
+</section>
