856f959ca78119530574adb0c7d81d4df07aa815
joshNM <josh@northernmediauk.com>
2017-05-26 10:08:01 +0100

    add load more styling

--- template-parts/content-employee-news.php
+++ template-parts/content-employee-news.php
@@ -6,26 +6,6 @@
         quis nostrud exercitation ullamco laboris nisi.</p>
 
         <div class="row">
-
-            <?php 
-                $employeeArgs = [
-                    'post_type' => 'post',
-                    'category__in' => [18]
-                ];
-
-                $employeeArticle = new WP_Query($employeeArgs);
-            ?>
-            <?php while($employeeArticle->have_posts()) : $employeeArticle->the_post(); ?>
-                <div class="col-md-4">
-                    <div class="employee-article">
-                    <img src="<?php the_post_thumbnail_url(); ?>">
-                    <div class="employee-article__content">
-                        <span><?php echo get_the_date() ?></span>
-                        <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-                    </div>
-                    </div>
-                </div>
-            <?php endwhile; ?>
         
         <?php echo do_shortcode('[ajax_load_more container_type="div" post_type="post" posts_per_page="3" category="employee-news" scroll="false"]') ?>
         
