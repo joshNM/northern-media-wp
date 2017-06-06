64f6d33ad8a0a38917372cdea59cdb3d3f378be8
joshNM <josh@northernmediauk.com>
2017-06-06 14:05:52 +0100

    change faqs to use flexbox

--- template-parts/content-faqs.php
+++ template-parts/content-faqs.php
@@ -2,16 +2,19 @@
     <div class="container">
         <center><h2 class="section-title"><?php the_title() ?> FAQs</h2></center>
         
-        <div class="row">
+        <div class="flex-faq">
 
         <?php
         // check if the repeater field has rows of data
         wp_reset_query();
         if( have_rows('faqs') ):
             // loop through the rows of data
+            $leftIndex = 0;
             while ( have_rows('faqs') ) : the_row();
         ?>  
-            <div class="col-md-6">
+            
+            <?php if($leftIndex < 3) : ?>
+            
                 <div class="Faq">
                     <div class="Faq__title">
                         <h3><?php the_sub_field('faq_title'); ?></h3>
@@ -20,7 +23,10 @@
                         <p><?php the_sub_field('faq_answer'); ?></p>
                     </div>
                 </div>
-            </div>
+        
+            <?php endif; ?>
+
+
         <?php
             endwhile;
         else :
