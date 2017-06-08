72b8809b17fe95fd87b8f20c58ea160dfe31bef1
joshNM <josh@northernmediauk.com>
2017-06-08 11:16:42 +0100

    new archive page to follow website design

--- archive.php
+++ archive.php
@@ -9,63 +9,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php get_template_part( 'template-parts/content', 'servicenav' ); ?>
-
-	<div class="archive" style="padding: 30px 0px 0px 0px; background: #c1ce02;">
-		
-		<style type="text/css">
-			h1, h2, h3, h4, p {
-				/*color: white;*/
-			}
-			
-			.latest-news-inner {
-				background: white;
-				padding: 22px;
-				margin-bottom: 30px;
-				-webkit-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
-				-moz-box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
-				box-shadow: 0px 0px 14px 0px rgba(0,0,0,0.75);
-			}
-
-		</style>		
-
-		<div class="container">
-			<h1 style="color: white; margin-top: 0px; text-align: center; margin-bottom: 30px;">Archive</h1>
-			<?php
-				if (have_posts()) :
-					while (have_posts()) : the_post() ?>
-			      <div class="col-xs-12 col-sm-6 col-md-6">
-			        <div class="latest-news-inner">
-			          <div class="content">
-			            <h2 style="margin-top: 0px;"><a href="<?php the_permalink(); ?>" style="color: black; text-decoration: none;"><?php the_title(); ?></a></h4>
-			            <p>
-			              <?php echo wp_trim_words( get_the_content(), 50, '...' ); ?>
-			            </p>
-			            <span><a href="<?php the_permalink(); ?>">Read more</a></span>
-			          </div>
-			        </div>
-			      </div>
-					<?php endwhile;
-				endif;
-
-			?>
-		</div>
-	</div>
-
-	<?php 
+	<?php
+		// Service nav
+		get_template_part( 'template-parts/content', 'servicenav' );
+		// Slider
+		get_template_part( 'template-parts/content', 'slider' );
+		// Latest news
+		get_template_part( 'template-parts/content', 'archive-news' );
+		// Case studies
+		get_template_part( 'template-parts/content', 'casestudy' );
+		// Approach
+		get_template_part( 'template-parts/content', 'approach' );
+		// Newsletter
 		get_template_part( 'template-parts/content', 'newsletter' );
 		// Contact
 		get_template_part( 'template-parts/content', 'contact' );
 
 		get_template_part( 'template-parts/content', 'social' );
 
-	?>
+		get_template_part( 'template-parts/content', 'map' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+
+	?>
 
 <?php
 get_footer();
