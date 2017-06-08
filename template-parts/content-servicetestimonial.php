16ee162796e8441833e8a0534175b7338311e778
joshNM <josh@northernmediauk.com>
2017-06-08 10:30:58 +0100

    testimonial slider

--- template-parts/content-servicetestimonial.php
+++ template-parts/content-servicetestimonial.php
@@ -15,7 +15,7 @@
                 <?php the_content(); ?>
                 </div>
                 <p class="author-of-testimonial"><?php the_title(); ?></p>
-                <p class="author-workplace"><?php the_field('testimonial_author'); ?></p>
+                <p class="author-workplace"><?php the_field('testimonial_workplace'); ?></p>
                 <div class="slide-bg"></div>
 
                 <div class="next">
